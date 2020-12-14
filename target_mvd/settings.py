6b675da22f5c89498530bfd3e76b2b0a3eecb723
Tiziana Romani <tiziana.romani@rootstrap.com>
2020-12-14 07:42:16 -0300

    use single quotation marks

--- target_mvd/settings.py
+++ target_mvd/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'target_mvd.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT"),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
