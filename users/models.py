1041401d795f9f8b0305fe2b2ee9e8d47bbbcf08
Tiziana Romani <tiziana.romani@rootstrap.com>
2020-12-15 11:05:49 -0300

    Django config and sign up feature. (#2)

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    class Gender(models.TextChoices):
+        MALE = 'M', ('Male')
+        FEMALE = 'F', ('Female')
+        NOT_SPECIFIED = 'NS', ('Not specified')
+    
+    gender = models.CharField(max_length=2, choices=Gender.choices)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
